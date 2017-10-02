d4669724a8b5ddc2318dba250130495233925f21
rtalwai <rtalwai@iu.edu>
2017-10-01 22:51:05 -0400

    added documentation

--- problem3/solver16.py
+++ problem3/solver16.py
@@ -3,7 +3,89 @@
 
 # put your 15 puzzle solver here!
 
+# Fall 2017, B-551 Elements of AI. Prof. Crandall
+# Assignment 1, Question 3
+# Raghottam Dilip Talwai, Siddharth Jayant Pathak and Yue Chen
 
+
+
+# The given problem is to solve the 15 puzzle using A* search technique.
+# In A* we use heuristics to find an optimal path to reach the goal state.
+# A heuristic has to be admissible and consistent to give an optimal path
+
+# Abstraction
+
+# Set of Valid States: Any state having 15 tiles in any order and a blank tile is a valid state.
+# Initial State:  A configuration of 4*4 board including any arrangement of  numbered tiles from 1 to 15 and a  blank tile.
+#                Each tile occupies exactly one place on the board.
+
+# Goal state: A sequential arrangement of tiles starting from 1 to 15 with each tile being having a unique position and
+#            the blank tile occupying the last position towards the right of the 15th tile.
+
+# Successor Function: This is a variant of the usual 15 puzzle problem wherein a tile can slide at the most 3 places in
+#                     left, right , up or down direction in a single move generating 6 possible successors for each state.
+#                    Each successor will have a cost and the path taken to reach from the parent state associated with it.
+
+# Edge Weights:-f(s)-  It is the sum of the cost of moving a tile and heuristic cost. f(s)= g(s)+h(s)
+#                      The Fringe will pop out the state with the lowest edge weight to get the successors of that state
+# Cost Function:-g(s)- The cost of moving one, two or three tiles is considered to be the same.
+
+# Heuristic Function:-h(s)- We are using linear conflict heuristic.
+#                                        h(s)= MD/3 + lc *2
+#                   where MD is the total Manhattan distance and lc is the number of linear conflicts.
+
+# Manhattan Distance- The Manhattan Distance is the distance between tile's current position and its goal position.
+#                    We  are taking the total Manhattan distance for all the tiles and dividing by 3 in order to account
+#                    for the fact that in a each move maximum 3 tiles can be moved.
+
+# Linear Conflict: Two tiles Ti and Tj are in a linear conflict if Ti and Tj are in the same line, the goal positions of
+#                 Ti and Tj  are both in that line, Ti is to the right of Tj and goal position of Ti is to the left of
+#                 the goal position of Tj.(Referred: https://heuristicswiki.wikispaces.com/Linear+Conflict)
+
+
+# For any given tile, it will have to travel at the least its Manhattan distance to reach its goal position.
+# But if it faces any other tile in its path leading into a linear conflict, then two moves will be added to its Manhattan distance as one of the two tiles would have to make way for the other so that both tiles reach their goal position.
+# In this case 2 moves will be added to the Manhattan distance of both tiles and both these tiles will have to travel at the least this distance moves to reach its goal.
+# The number of moves required would always be greater than the heuristic value. Hence linear conflict heuristic will never overestimate the actual number of moves required and will be admissible.
+
+# Search Algorithm
+
+# We have used an algorithm based on the search algorithm 3 for implemeting A*.
+# Initial state is checked for the the goal condition. If not then it is added to the fringe
+# We have used fringe as s heap structure . The fringe will by default pop the state having the lowest cost first.
+# As soon as a state is popped, it is checked for the goal condition, if goal is reached, the path containing moves from the initial state is returned.
+# Otherwise the state is checked if it is already in the visited list. If so,then the next state with lowest cost is popped from the fringe.
+# If the state is not visited, then it is added to the visited list and is expanded to get its successors.
+# The heuristic function is called and the total cost of reaching the goal state is calculated.
+# If a state is already existing in the fringe with a cost higher than the current cost, then the higher cost is ignored and the state is stored in the fringe with the current lower cost.
+# If a state doesnt exists in the fringe, it is added to the fringe along with its associated cost
+# The steps are repeated until the goal state is found.
+
+# Problems Faced
+
+# The selection of the correct heuristic function was a big task. For problems such as the 15 puzzle, getting an optimal solution entirley depends on how strong the heuristic is.
+# We tried first using the Manhattan distance/3 as the heuristic function. But though it was giving a solution yet we found it is weak compared to linear conflict heuristic,
+# as for the same input state we got a solution in less number of moves.
+
+# Though linear conflict heuristic is admissible and strong compared to other, yet we could not get solution for input states requiring large number of moves.
+# We tried executing the code for different test cases and at the most the code could solve a puzzle requiring 17 moves to reach the goal state within 3 mins.
+
+
+# Design Decisions:
+
+# Decided to use search algorithm 3 as this problem will have a large state space and there has to be some mechanism based on which only optimal states are expanded and
+# unecessary states are ignored. Using search algorithm 3 served this purpose.
+
+# Used a heapq for fringe. The total cost and the states are stored in a single tupple. The fringe being a heapq will automatically pop out the state with the lowest cost.
+# This avoids sorting states in the fringe according to the cost.
+
+# Also implemented a check_parity function which will calculate the number of inversions on the input state and will return if the state is solvable or not.
+# Referred: http://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+
+
+
+
+#SOURCE CODE
 
 import sys
 import copy
@@ -11,22 +93,18 @@ import timeit
 import heapq
 
 
-
-
 goal_state=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]]
-s=[]
-visited=[]
-
+s=[] #stores the list of successors. It is a list of lists which each list consisting of the state and the moves taken to reach that state
+visited=[] #stores the list of visited states
 
 #To move blank tile in the horizontal direction
 def move_horizontal(G,i,j,path):
-    
+
     move_left=copy.deepcopy(G)
     move_right=copy.deepcopy(G)
-    tiles_moved=0
+    tiles_moved=0 #To store the number of tiles moved, can be 1,2 or 3
 
     #To move right
-
     right = j-1
     while right >=0:
         move_right[i][right],move_right[i][right+1]=move_right[i][right+1],move_right[i][right]
@@ -37,11 +115,11 @@ def move_horizontal(G,i,j,path):
         else:
             moves= path+" "+'R'+str(tiles_moved)+str(i+1)
         s2=[s1,moves]
+        #Adding states to the successors list
         s.append(s2)
         right=right-1
 
     #To move Left
-
     left = j+1
     tiles_moved=0
     while(left<4):
@@ -56,15 +134,12 @@ def move_horizontal(G,i,j,path):
         s2=[s1,moves]
         s.append(s2)           
 
-
 #To move blank tile in the vertical direction
-
 def move_vertical(G,i,j,path):
     move_up=copy.deepcopy(G)
     move_down=copy.deepcopy(G)
 
     #To move Down
-
     down = i-1
     tiles_moved = 0
     while down>=0 :
@@ -103,7 +178,6 @@ def Linear_conflict(LC):
     for row in range(4):
         max = -1
         for col in range(4):
-
             if LC[row][col] != 0 and int((LC[row][col] - 1) / 4) == row:
 
                 if LC[row][col] > max:
@@ -114,7 +188,6 @@ def Linear_conflict(LC):
     for col in range(4):
         max = -1
         for row in range(4):
-
             if LC[row][col] != 0 and int((LC[row][col] % 4)) == (col + 1)%4 :
 
                 if LC[row][col] > max:
@@ -127,7 +200,7 @@ def Linear_conflict(LC):
     return conflicts
 
 
-#Manhattan distance
+# Calculates Manhattan distance
 def manhattan_heuristic(MD):
     heuristic=0
     for x in range(4):
@@ -170,7 +243,6 @@ def check_infringe(succ,fringe):
     else:
         
         for i,j in enumerate(fringe):
-            
             if succ == j[1][0]:
 
                 return(i,j[0],True)
@@ -189,7 +261,7 @@ def solve(matrix):
         #Popping lowest cost state
         popped=heapq.heappop(fringe)
 
-        #print("popping with total_cost= %f: "%popped[0])
+        #To store the state popped
         state=popped[-1][0]
 
         #Storing Moves
@@ -200,6 +272,7 @@ def solve(matrix):
             print("No of nodes visitied before reaching goal: %d" % (len(visited) - 1))
             return (path)
 
+        #To check if state is already in the visited list
         if state in visited:
             continue
         else:
@@ -210,28 +283,28 @@ def solve(matrix):
             cost= popped[-1][2]+1
 
             for succ in successors(state, path):
-                #if succ[0] in visited:
-                    #continue
+
                 succ.append(cost)
+
                 #Calculating heuristic
                 total_cost = manhattan_heuristic(succ[0])+cost
                 index,h1,in_fringe= check_infringe(succ[0],fringe)
 
-                #Checking if a larger s' exists in fringe
+                #Checking if a state exists with a larger cost in the fringe
                 if in_fringe == True and h1 > total_cost:
                     del fringe[index]
                     heapq.heapify(fringe)
                     heapq.heappush(fringe,(total_cost,succ))
 
                 elif in_fringe == False:
-                    #Append successor in the fringe
+                    #Append successor in the fringe with its total cost
                     heapq.heappush(fringe,(total_cost,succ))
             
             #Clearing the successors list for new iteration
             del s[:]
     return False
 
-#To calculate inversions of a particular input state
+#To calculate inversions of a particular input state. Read about it on http://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
 def check_parity(initial_puzzle):
 
     parity=[ele for l in initial_puzzle for ele in l]
@@ -252,11 +325,11 @@ def check_parity(initial_puzzle):
     else:
         return False
 
-# Main
 
-filename='input1.txt'
-#filename=sys.argv[1]
-initial_state=[]
+# Main
+filename=sys.argv[1]
+
+#Reads input from file
 with open(filename,'r') as b1:
     initial_state = [[int(i) for i in line.split()] for line in b1]
 
@@ -275,4 +348,3 @@ else:
     
     
 
-:
